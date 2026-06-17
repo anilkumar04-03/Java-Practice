0b16d3a3cacb4d69f4c0d468d19f7eed0b6f0ff8
AnilSharma004 <editorxanil@gmail.com>
2026-06-17 22:06:10 +0530

    Added dynamic method dispatch example

---
+++ OOP/DynamicMethodDispatch.java
@@ -0,0 +1,32 @@
+class Phone {
+    public void greet() {
+        System.out.println("Good Morning");
+    }
+
+    public void on() {
+        System.out.println("My name is Java");
+    }
+}
+
+class SmartPhone extends Phone {
+    public void welcome() {
+        System.out.println("Welcome Sir");
+    }
+
+    @Override
+    public void on() {
+        System.out.println("Turning on Smartphone....");
+    }
+}
+
+public class DynamicMethodDispatch {
+    public static void main(String[] args) {
+
+        Phone phone = new SmartPhone();
+        SmartPhone smartPhone = new SmartPhone();
+
+        phone.greet();
+        phone.on();
+        smartPhone.welcome();
+    }
+}
